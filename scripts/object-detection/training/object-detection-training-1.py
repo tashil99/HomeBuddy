c3c9d0568e1b3995ad25ea398e25e6c51baf71b6
Tanuj <tanuj.motee07@gmail.com>
2025-11-27 09:58:07 +0400

    2nd Model - trainings 1&2

--- scripts/object-detection/training/object-detection-training-1.py
+++ scripts/object-detection/training/object-detection-training-1.py
@@ -12,7 +12,7 @@ def main():
     os.environ["ULTRALYTICS_CACHE"] = "ram"  # speed up by caching to RAM
 
     # Load lightweight YOLOv8 model
-    model = YOLO("yolov8n.pt")
+    model = YOLO("yolov5n.pt")
 
     # Train (GPU-optimized config)
     results = model.train(
@@ -27,7 +27,7 @@ def main():
     )
 
     # Save results
-    results_dir = "runs/detect/object-detection-model-1"
+    results_dir = "runs/detect/object-detection-model-10"
     os.makedirs(results_dir, exist_ok=True)
     results_path = os.path.join(results_dir, "results.pkl")
     with open(results_path, "wb") as f:
